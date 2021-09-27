4c742c480060501b315975e51f303a14494ab1f7
loading8425 <50039686+loading8425@users.noreply.github.com>
2021-09-27 16:13:14 +0800

    Update test_app.py

--- CI-Test/test_app.py
+++ CI-Test/test_app.py
@@ -1,4 +1,16 @@
 import app
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+#response = requests.get(BASE + "test/Jack")
+
+response = requests.put(BASE + "test/Jack", {"someData":"hello", "exampleData": 100})
+
+#response = requests.post(BASE + "test/jack")
 
 def test():
   assert app.home() != "HomePage"
+  
+  response = requests.put(BASE + "test/Jack", {"someData":"hello", "exampleData": 100})
+  assert response != {"someData":"hello", "exampleData": 100}
