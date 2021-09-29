e44a866db7e72847bc0855e0b23c39c2ad3759d1
Mingyang LIU <szej18@gmail.com>
2021-09-29 19:52:05 +0800

    Test frontend deploy

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend/build")
 app.config['SECRET_KEY'] = 'ITPROJECT'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
