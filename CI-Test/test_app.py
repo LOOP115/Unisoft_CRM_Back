33e75f466e3a65788d4858ee002f27cfe2699c7a
Mingyang LIU <szej18@gmail.com>
2021-09-29 20:06:30 +0800

    Update Procfile

--- CI-Test/test_app.py
+++ CI-Test/test_app.py
@@ -10,16 +10,20 @@ def test_register():
   response = requests.post(BASE + "register", json={"username":"test", "email": "test@test.com", "password":123456})
   assert response.status_code!=404 and response.status_code!=500
 
+def test_home():
+  response = requests.get(BASE + "home")
+  assert response.status_code != 404
+
+def test_put():
+  response = requests.put(BASE + "test/Jack", {"someData":"hello", "exampleData": 100})
+  assert response.status_code!=404
+
+def test_register():
+  r = {"username":"d32e1dsa", "email": "adaes12n@uni.com", "password":12345}
+  response = requests.post(BASE + "register", json= r)
+  assert response.status_code!=404
+
 def test_login():
-  r = {"email": "test@test.com", "password":"123456"}
-  response = requests.post(BASE + "login", json= r)
-  assert response.status_code!=404 and response.status_code!=500
-
-# test whether registered account can log in or not
-def test_valid_login():
-  response = requests.post(BASE + "register", json={"username":"test", "email": "test@test.com", "password":123456})
-  if(response.status_code == 500 or response.status_code == 404):
-    assert response.status_code!=404 and response.status_code!=500
-  
-  requests.post(BASE + "login", json={"username":"test", "email": "test@test.com", "password":123456})
-
+  r = {"email": "adaes12n@uni.com", "password":12345}
+  response = requests.post(BASE + "register", json= r)
+  assert response.status_code!=404
