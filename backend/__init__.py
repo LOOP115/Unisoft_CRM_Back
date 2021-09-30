118d0ffb8804e1cfb8afa7b3ce7affb5f53837f6
LOOP <54584749+LOOP115@users.noreply.github.com>
2021-09-30 11:52:56 +0800

    Update new mail

--- backend/__init__.py
+++ backend/__init__.py
@@ -23,8 +23,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "cjhm000115@gmail.com"
-app.config['MAIL_PASSWORD'] = "nuinqttzqjbhnxkw"
+app.config['MAIL_USERNAME'] = "unisoft6969@gmail.com"
+app.config['MAIL_PASSWORD'] = "unisoftgei"
 mail = Mail(app)
 
 
