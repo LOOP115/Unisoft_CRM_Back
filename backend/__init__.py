eaff4878bd84aff55df00e900567decac00718bd
LOOP <54584749+LOOP115@users.noreply.github.com>
2021-10-16 12:41:48 +0800

    Solve cookies bug

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,10 @@
-import os
-from flask import Flask
+from flask import Flask, session
+from flask.sessions import SecureCookieSessionInterface
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-#from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ITPROJECT'
@@ -14,10 +13,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 api = Api(app)
-#CORS(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+session_cookie = SecureCookieSessionInterface().get_signing_serializer(app)
+
+
+@app.after_request
+def cookies(response):
+    same_cookie = session_cookie.dumps(dict(session))
+    response.headers.add("Set-Cookie", f"session={same_cookie}; Secure; HttpOnly; SameSite=None; Path=/;")
+    return response
+
 
 # Mail Config
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
@@ -26,7 +33,6 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "unisoft6969@gmail.com"
 app.config['MAIL_PASSWORD'] = "innrgezfveffibgt"
 mail = Mail(app)
-
 
 from backend import routes
 from backend import apis
--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,6 @@
 import json
 from datetime import datetime
-from flask import jsonify, json
-from flask import request, abort
+from flask import jsonify, json, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 
